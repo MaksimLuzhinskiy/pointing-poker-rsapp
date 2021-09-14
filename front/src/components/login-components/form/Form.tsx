fcb69833ae1007f95f26d9f725ba0b4972247a7c
MaksimLuzhinskiy <75784097+MaksimLuzhinskiy@users.noreply.github.com>
2021-09-14 13:25:23 +0300

    styled-component in constats.ts

--- front/src/components/login-components/form/Form.tsx
+++ front/src/components/login-components/form/Form.tsx
@@ -11,24 +11,7 @@ import ButtonConfirm from './buttonConfirm/ButtonConfirm';
 import ButtonClose from './buttonClose/ButtonClose';
 import Switcher from './switcher/Switcher';
 import { useTranslation } from 'react-i18next';
-
-const WrapForm = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: rgba(196, 196, 196, 0.6);
-  left: 0;
-  top: 0;
-`;
-const WrapFlex = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const WrapSwitcher = styled.div`
-  display: flex;
-  align-items: center;
-`;
+import WrapForm, { WrapFlex, WrapSwitcher } from './constans';
 
 export interface IStateForm {
   firstName: string;
