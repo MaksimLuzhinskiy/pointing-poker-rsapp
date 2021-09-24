dfe344573b0cab61aaea8d7038b9725db74bffe4
Dovoyan <70629154+Dovoyan@users.noreply.github.com>
2021-09-24 16:26:14 +0300

    add funcionality

--- front/src/components/chat-componets/chat-component.tsx
+++ front/src/components/chat-componets/chat-component.tsx
@@ -7,7 +7,7 @@ const ChatComponents = (props) => {
       <div className="chatComponents">
         <div className="chatText chatComponent">{props.message}</div>
         <div className="chatUser chatComponent">
-          <img className="chatIcon" src="../channels4_profile.jpg" />
+          <img className="chatIcon" src={props.image} />
           <div className="chatUser__text">{props.name}</div>
           <img className="chatBan" src="../images.png" />
         </div>
--- front/src/components/chat-componets/chat.tsx
+++ front/src/components/chat-componets/chat.tsx
@@ -1,36 +1,73 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ChatComponents from './chat-component';
 import './chat.scss';
 
-const employees = [
-  {
-    id: 1,
-    Name: 'Perega',
-    message:
-      'Я разорванный на части как на праздник с кремом торт стратегически на скилле меня обыграл минер...',
-    image: 'Михайлович',
-  },
-  {
-    id: 1,
-    Name: 'Perega',
-    message:
-      'Я разорванный на части как на праздник с кремом торт стратегически на скилле меня обыграл минер...',
-    image: 'Михайлович',
-  },
-];
+// const employees = [
+//   {
+//     id: 1,
+//     Name: 'Perega',
+//     message:
+//       'Я разорванный на части как на праздник с кремом торт стратегически на скилле меня обыграл минер...',
+//     image: 'Михайлович',
+//   },
+//   {
+//     id: 1,
+//     Name: 'Perega',
+//     message:
+//       'Я разорванный на части как на праздник с кремом торт стратегически на скилле меня обыграл минер...',
+//     image: 'Михайлович',
+//   },
+// ];
 
 const Chat = () => {
   const inputEl = useRef(document.createElement('input'));
+  const chat = useRef(document.createElement('div'));
+
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      name: 'Perega',
+      message:
+        'Я разорванный на части как на праздник с кремом торт стратегически на скилле меня обыграл минер...',
+      image: '../9jXFIYtcQe0.jpg',
+    },
+    {
+      id: 2,
+      name: 'Максиm',
+      message: 'Я ne лох',
+      image: '../images.png',
+    },
+  ]);
 
   function sendMessage() {
     console.log(inputEl.current.value);
+    setMessages([
+      ...messages,
+      {
+        id: messages.length + 1,
+        name: 'Yan',
+        message: inputEl.current.value,
+        image: '../channels4_profile.jpg',
+      },
+    ]);
+    inputEl.current.value = '';
   }
+
+  useEffect(() => {
+    // Обновляем заголовок документа с помощью API браузера
+    chat.current.scrollTop = chat.current.scrollHeight;
+  });
 
   return (
     <div className="chatArea chatInvisibility">
-      <div className="chatField">
-        {employees.map((employee) => (
-          <ChatComponents name={employee.Name} message={employee.message} />
+      <div ref={chat} className="chatField">
+        {messages.map((mesage) => (
+          <ChatComponents
+            key={mesage.id}
+            name={mesage.name}
+            message={mesage.message}
+            image={mesage.image}
+          />
         ))}
       </div>
       <div>
